956fb8a2b10fd4d1fdcecff670c51bd5d92300a5
davidventa89 <dvent014@fiu.edu>
2022-03-28 21:51:36 -0400

    Added GetStarShips() method

--- SWAPI_Exercise/Program.cs
+++ SWAPI_Exercise/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
 
 namespace SWAPI_Exercise
 {
@@ -10,6 +12,28 @@ namespace SWAPI_Exercise
     {
         static void Main(string[] args)
         {
+            GetStarShips();
+        }
+
+        public static void GetStarShips()
+        {
+            string url = "https://swapi.dev/api/";
+
+            var starships = new RestClient(url);
+            var request = new RestRequest("starships", Method.Get);
+            request.AddHeader("Accept", "application/json");
+            request.RequestFormat = DataFormat.Json;
+
+            var response = starships.ExecuteAsync(request);
+
+            string content = response.Result.Content;
+            
+            dynamic JsonResult = JsonConvert.DeserializeObject(content);
+
+            foreach(var item in JsonResult.results)
+            {
+                Console.WriteLine("");             
+            }
         }
     }
 }
